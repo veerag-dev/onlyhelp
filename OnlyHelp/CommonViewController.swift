2de5b9b11bc90cfddfd4b8b361bb9a8f9434ad4d
veera <veera@bytemark.co>
2020-02-24 12:11:05 +0530

    removed extra lines

--- OnlyHelp/CommonViewController.swift
+++ OnlyHelp/CommonViewController.swift
@@ -20,16 +20,4 @@ class CommonViewController: UIViewController {
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
